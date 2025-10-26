0a1266073287565904ff49465793a83fe111ddff
Dmitry Permyakov <taren4ik@mail.ru>
2025-10-26 23:57:18 +1000

    add on_configuration_change and indexes

--- dbt_course_practice/models/staging/flights/stg_flights_bookings.sql
+++ dbt_course_practice/models/staging/flights/stg_flights_bookings.sql
@@ -1,12 +1,20 @@
 {{
       config(
-      materialized = 'table'
+      materialized = 'materialized_view',
+      on_configuration_change="apply",
+      indexes=[
+        {
+            "columns": ["book_date"],
+            "unique":  false
+        }
+    ]
       )
 }}
 
 select
     book_ref,
     book_date,
-    total_amount
+    total_amount,
+    total_amount * 2 as full_amount
 from
     {{source('demo_src', 'bookings') }}
